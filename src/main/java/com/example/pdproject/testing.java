7e93f19e80fa34baf82165c5cd9244d9fa1b1274
ULTRA-L <loydvendiola@gmail.com>
2023-06-24 23:56:42 +0800

    Add SQL Connection, some practice files

---
+++ src/main/java/com/example/pdproject/testing.java
@@ -0,0 +1,18 @@
+package com.example.pdproject;
+
+import com.main.Game.SQLConnect;
+
+import java.sql.SQLException;
+
+public class testing {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        SQLConnect sql = new SQLConnect("jdbc:mysql://localhost:3306/english_quiz","english_quiz.easy");
+        String temp[][] = sql.generateQuestion(10);
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[j].length; j++) {
+                System.out.print(temp[i][j] + "\t|\t");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.pdproject {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens com.example.pdproject to javafx.fxml;
     exports com.example.pdproject;
