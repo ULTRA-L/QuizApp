adadf17838cd4240f5b0dfb52ef8dc7915633426
ULTRA-L <loydvendiola@gmail.com>
2023-06-25 16:20:22 +0800

    rename

--- src/main/java/com/main/Game/ChoiceGame.java
+++ src/main/java/com/main/Game/ChoiceGame.java
@@ -3,7 +3,7 @@ package com.main.Game;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class MathGame extends Application {
+public class ChoiceGame extends Application {
 
     public static void main(String[] args) {
         launch(args);
