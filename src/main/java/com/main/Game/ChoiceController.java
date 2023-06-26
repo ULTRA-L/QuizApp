e3c9e78604da7e929079fd91fb833bce27f33f4d
ULTRA-L <loydvendiola@gmail.com>
2023-06-26 16:53:47 +0800

    ChoiceGame updated for testing. SQL Imported to Main

--- src/main/java/com/main/Game/ChoiceController.java
+++ src/main/java/com/main/Game/ChoiceController.java
@@ -1,4 +1,91 @@
 package com.main.Game;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class ChoiceController {
+
+    @FXML
+    private Label question_label;
+    @FXML
+    private Label question_number;
+    @FXML
+    private RadioButton btn_A;
+    @FXML
+    private RadioButton btn_B;
+    @FXML
+    private RadioButton btn_C;
+    @FXML
+    private Button next_btn;
+    @FXML
+    private ProgressBar prog;
+    @FXML
+    private Label time;
+    private int q_num = 1;
+    private String[][] questions;
+    private int num_of_questions = 10;
+    private ToggleGroup toggleGroup;
+    private int score = 0;
+
+    @FXML
+    protected void initialize() throws SQLException, ClassNotFoundException {
+        SQLConnect sql = new SQLConnect("jdbc:mysql://localhost:3306/general_quiz","easy");
+
+        toggleGroup = new ToggleGroup();
+        btn_A.setToggleGroup(toggleGroup);
+        btn_B.setToggleGroup(toggleGroup);
+        btn_C.setToggleGroup(toggleGroup);
+
+        questions = sql.generateQuestion(num_of_questions);
+        System.out.println("Question ID: "+questions[0][0]);
+
+        question_number.setText("Question Number " + q_num);
+        question_label.setText(questions[0][1]);
+        btn_A.setText(questions[0][2]);
+        btn_B.setText(questions[0][3]);
+        btn_C.setText(questions[0][4]);
+        //question_number.setText("test Question number");
+        //question_label.setText("Test question label");
+        //btn_A.setText("Choice A");
+        //btn_B.setText("Choice B");
+        //btn_C.setText("Choice C");
+    }
+    @FXML
+    protected void onNextButtonClick(){
+        if(toggleGroup.getSelectedToggle() == null){
+            System.out.println("Unselect");
+            return;
+        }
+        System.out.println(((RadioButton)toggleGroup.getSelectedToggle()).getText() + " | "+questions[q_num-1][5]);
+        if((((RadioButton)toggleGroup.getSelectedToggle()).getText()).equals(questions[q_num-1][5])){
+            score++;
+            System.out.println("Score++");
+        }
+
+        if(q_num >= num_of_questions){
+            System.out.println("END. Score: "+score);
+            Stage stage = (Stage) next_btn.getScene().getWindow();
+            stage.close();
+            return;
+        }
+
+        toggleGroup.getSelectedToggle().setSelected(false);
+        System.out.println("Question ID: "+questions[q_num][0]);
+        question_label.setText(questions[q_num][1]);
+        btn_A.setText(questions[q_num][2]);
+        btn_B.setText(questions[q_num][3]);
+        btn_C.setText(questions[q_num][4]);
+        q_num++;
+        question_number.setText("Question Number " + q_num);
+        if(q_num == num_of_questions){
+            next_btn.setText("END");
+        }
+
+    }
+
+
+
+
 }
--- src/main/java/com/main/Game/ChoiceGame.java
+++ src/main/java/com/main/Game/ChoiceGame.java
@@ -1,16 +1,24 @@
 package com.main.Game;
 
+import com.example.pdproject.UITesting;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ChoiceGame extends Application {
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(UITesting.class.getResource("ChoiceGameUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),453,400);
+        primaryStage.setTitle("Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
