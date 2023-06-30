f2c51d0c1e24fcbe990064649eee530d5e219375
ULTRA-L <loydvendiola@gmail.com>
2023-06-30 08:05:11 +0800

    Connected both english and  general quiz

--- src/main/java/com/main/Game/DifficultyMenuController.java
+++ src/main/java/com/main/Game/DifficultyMenuController.java
@@ -2,6 +2,7 @@ package com.main.Game;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
@@ -20,20 +21,39 @@ public class DifficultyMenuController {
 
     @FXML
     private AnchorPane difficultyMenuAnchor;
+    private final SceneSwitch ss = new SceneSwitch();
 
     @FXML
     void chooseEasy(ActionEvent event) throws IOException {
-        new SceneSwitch("easy",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        if(ss.getTopic() == "jdbc:mysql://localhost:3306/english_quiz"){
+            new SceneSwitch("easy",difficultyMenuAnchor, "EnglishQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/general_quiz"){
+            new SceneSwitch("easy",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/math_quiz"){
+            new SceneSwitch("easy",difficultyMenuAnchor, "MathQuizView.fxml");
+        }
     }
 
     @FXML
     void chooseHard(ActionEvent event) throws IOException {
-        new SceneSwitch("hard",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        if(ss.getTopic() == "jdbc:mysql://localhost:3306/english_quiz"){
+            new SceneSwitch("hard",difficultyMenuAnchor, "EnglishQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/general_quiz"){
+            new SceneSwitch("hard",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/math_quiz"){
+            new SceneSwitch("hard",difficultyMenuAnchor, "MathQuizView.fxml");
+        }
     }
 
     @FXML
     void chooseMedium(ActionEvent event) throws IOException {
-        new SceneSwitch("medium",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        if(ss.getTopic() == "jdbc:mysql://localhost:3306/english_quiz"){
+            new SceneSwitch("medium",difficultyMenuAnchor, "EnglishQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/general_quiz"){
+            new SceneSwitch("medium",difficultyMenuAnchor, "GeneralQuizView.fxml");
+        }else if(ss.getTopic() == "jdbc:mysql://localhost:3306/math_quiz"){
+            new SceneSwitch("medium",difficultyMenuAnchor, "MathQuizView.fxml");
+        }
     }
 
 }
